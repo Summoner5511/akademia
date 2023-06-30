83380c760fbfd5e6cf19a9df0b7724247d5be065
Summoner5511 <138057679+Summoner5511@users.noreply.github.com>
2023-06-30 15:46:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-
 <?php
-date_default_timezone_set('Europe/Bratislava');
+date_default_timezone_set("Europe/Bratislava"); 
 print 'Ahoj <br>';
-$date = date('d/m/Y', time());
-$time = date('H:i:s', time());
+$date = date('d/m/Y');
+$time = date('H:i:s');
 print 'Teraz je ' . $time . ' | ' . $date;
-
 if (date('H') > 8) {
-    print '<br> Meškanie';
+    print '<br> Meškáš!';
 }
+
+// LOGGER
+$logger = fopen("logger.log", "a");
+if (date('H') <= 8) {
+    fwrite($logger, $date . " | " . $time . "\n");
+}
+if (date('H') > 8) {
+    fwrite($logger, $date . " | " . $time . "  Meškanie" . "\n");
+}
+fclose($logger);
