806e6b1d251e5fd6f147d306abcb9361050426c8
Summoner5511 <138057679+Summoner5511@users.noreply.github.com>
2023-07-04 17:25:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,49 @@
+<form method="post" action="index.php">
+    Meno: <input type="text" name="name">
+    <input type="submit">
+</form>
+
+
 <?php
-date_default_timezone_set("Europe/Bratislava"); 
+date_default_timezone_set("Europe/Bratislava");
 print 'Ahoj <br>';
 $date = date('d/m/Y');
 $time = date('H:i:s');
 $date_time = $date . " | " . $time;
-print 'Teraz je ' . $time . ' | ' . $date;
+print 'Teraz je ' . $date_time;
 if (date('H') >= 8) {
     print '<br> Meškáš!';
 }
 
 // LOGGER
+$name = $_POST['name'];
 
-if (date('H') >= 8) {
-    file_put_contents("logger.log", $date_time . " Meškanie" . "\n", FILE_APPEND);
+function logger($date_time,$name)
+{
+    if (empty($name)) {
+        echo '<br>Napíš svoje meno';
+    } else {
+        if (date('H') >= 8) {
+            file_put_contents("logger.log", $date_time . " Meškanie " . "| Meno: " . $name . "\n", FILE_APPEND);
+        } else {
+            file_put_contents("logger.log", $date_time . "| Meno: " . $name .  "\n", FILE_APPEND);
+        }
+    }
 }
-else {
-    file_put_contents("logger.log", $date_time ."\n", FILE_APPEND);
+logger($date_time,$name);
+
+?>
+<br>
+<br>
+<br>
+<?php
+// LOGGER READER
+function getLogs(){
+    $logs = file_get_contents('logger.log')or die('Log súbor neexistuje');
+    $formattedlogs = nl2br($logs);
+    print $formattedlogs ;
 }
+getLogs();
+
+?>
+
