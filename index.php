c247e442e06ca5aa872fcf017d0b2d4104ee9154
Summoner5511 <138057679+Summoner5511@users.noreply.github.com>
2023-08-09 16:04:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,89 +4,92 @@
 </form>
 <?php
 date_default_timezone_set("Europe/Bratislava");
-$date = date('d/m/Y');
-$time = date('H:i:s');
-$date_time = $date . " | " . $time;
-welcome($date_time);
-function welcome($date_time)
+class logOfStudents
 {
-    print 'Ahoj <br>';
-    print 'Teraz je ' . $date_time;
-    checkTimeAndName($date_time);
-    getLogs();
-}
 
-/*          
-FUNCTIONS
-
-*/
-
-function checkTimeAndName($date_time)
-{
-    if (date('H') >= 20) {
-        print '<br>Príchod sa nemôže zapísať.';
-        exit;
-    }
-    if (date('H') >= 8) {
-        print '<br> Meškáš!';
-    }
-    if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        $name = $_POST['name'];
-
+        /*          
+        FUNCTIONS
+        
+        */
+        
+        public function welcome($date_time)
+        {
+            $date_time = date('d/m/Y H:i:s');
+            print 'Ahoj <br>';
+            print 'Teraz je ' . $date_time;
+            $this->checkTimeAndName($date_time);
+            $this->getLogs();
+        }
+        public function checkTimeAndName($date_time)
+    {
+        if (date('H') >= 20) {
+            print '<br>Príchod sa nemôže zapísať.';
+            exit;
+        }
+        if (date('H') >= 8) {
+            print '<br> Meškáš!';
+        }
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $name = $_POST['name'];
+        } elseif (isset($_GET['meno'])) {
+            $name = $_GET['meno'];
+        }
         if (empty($name)) {
             print '<br>Napíš svoje meno';
         } else {
-            loggerOfStudents($date_time, $name);
+            $this->loggerOfStudents($date_time, $name);
         }
     }
-}
-function loggerOfStudents($date_time, $name)
-{
-    if (empty($name)) {
-        return '<br>Napíš svoje meno';
-    }
-    $logMessage = $date_time . " | Meno: " . $name;
-    if (date('H') >= 8) {
-        file_put_contents("logger.log", $logMessage . " | Meškanie \n", FILE_APPEND);
-    } else {
-        file_put_contents("logger.log", $logMessage .  "\n", FILE_APPEND);
-    }
+    public function loggerOfStudents($date_time, $name)
+    {
+        if (empty($name)) {
+            return '<br>Napíš svoje meno';
+        }
+        $logMessage = $date_time . " | Meno: " . $name;
+        if (date('H') >= 8) {
+            file_put_contents("logger.log", $logMessage . " | Meškanie \n", FILE_APPEND);
+        } else {
+            file_put_contents("logger.log", $logMessage .  "\n", FILE_APPEND);
+        }
 
-    $jsonData = file_get_contents("names.json");
-    $names = array();
-    if ($jsonData) {
-        $names = json_decode($jsonData, true);
-    }
-    if (date('H') >= 8) {
-        $names[] = array(
-            'meno' => $name,
-            'order' => count($names) + 1,
-            'late' => 'yes'
-        );
-    } else {
-        $names[] = array(
-            'meno' => $name,
-            'order' => count($names) + 1,
-            'late' => 'no'
-        );
-    }
+        $jsonData = file_get_contents("names.json");
+        $names = array();
+        if ($jsonData) {
+            $names = json_decode($jsonData, true);
+        }
+        if (date('H') >= 8) {
+            $names[] = array(
+                'meno' => $name,
+                'order' => count($names) + 1,
+                'late' => 'yes'
+            );
+        } else {
+            $names[] = array(
+                'meno' => $name,
+                'order' => count($names) + 1,
+                'late' => 'no'
+            );
+        }
 
-    file_put_contents("names.json", json_encode($names, JSON_PRETTY_PRINT));
-}
-function getLogs()
-{
-    print "<pre>";
-    $logs = file_get_contents('logger.log') or die('Log súbor neexistuje');
-    print $logs;
-    print "</pre>";
-    print "<pre>";
-    $names = file_get_contents('names.json') or die('Súbor s menami neexistuje');
-    $student = json_decode($names, true);
-
-    $namesInJson = '';
-    foreach ($student as $box) {
-        $namesInJson .= '| Meno:' . $box['meno'] . ' , Poradie:' . $box['order'] . ' , Late: ' . $box['late'] . '|<br>';
+        file_put_contents("names.json", json_encode($names, JSON_PRETTY_PRINT));
     }
-    print $namesInJson;
-    print "</pre>";
+    public function getLogs()
+    {
+        print "<pre>";
+        $logs = file_get_contents('logger.log') or die('Log súbor neexistuje');
+        print $logs;
+        print "</pre>";
+        print "<pre>";
+        $names = file_get_contents('names.json') or die('Súbor s menami neexistuje');
+        $student = json_decode($names, true);
+
+        $namesInJson = '';
+        foreach ($student as $info) {
+            $namesInJson .= '| Meno:' . $info['meno'] . ' , Poradie:' . $info['order'] . ' , Late: ' . $info['late'] . '|<br>';
+        }
+        print $namesInJson;
+        print "</pre>";
+    }
 }
+$welcome = new logOfStudents;
+$welcome->welcome($date_time);
